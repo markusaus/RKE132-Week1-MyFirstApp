3734091460080ce4dca6bb187631b0381768162e
Markus <markus.aus@tktk.ee>
2024-09-23 15:30:56 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Enter your name:");
+string username;
+
+username=Console .ReadLine();
+
+Console.WriteLine($"Hello, {username}!");
